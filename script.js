bb9aa0c55488f326a5302bf26a9d166dd5bcebb4
Abhijit <abhijit.bhagavati1111@gmail.com>
2022-06-19 15:07:22 +0530

    -l1

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 setInterval(function(){
     var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
-    if(skipButton != undefined && skipButton.length>0){
+    if(skipButton != undefined && skipButton.length > 0){
         console.log("Ad detected");
         skipButton[0].click();
     }
 
 },1);
 
-chrome=require("chrome")
+chrome=require("chrome") // may have to remove this line to deploy the extension
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       return {cancel: details.url.indexOf("://www.evil.com/") != -1};
