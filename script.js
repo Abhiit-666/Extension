b900b169365f748aab6052f21bd724199ce80743
Abhijit <abhijit.bhagavati1111@gmail.com>
2022-06-19 12:43:49 +0530

    lenght correction

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setInterval(function(){
     var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
-    if(skipButton != undefined && skipButton.length()>0){
+    if(skipButton != undefined && skipButton.length>0){
         console.log("Ad detected");
         skipButton[0].click();
     }
